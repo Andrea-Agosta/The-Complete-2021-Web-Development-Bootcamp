aa58ca49a3e44ef297bab74d5d0fc7f95f1a9970
Andrea-Agosta <82880493+Andrea-Agosta@users.noreply.github.com>
2021-07-12 12:15:19 +0100

    useState Hook Practice

---
+++ usestate-hook-practice-forked/src/useState_Hook-Practice/src/components/App.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+function App() {
+  const [currentTime, setCurrentTime] = useState("Time");
+
+  function getTime() {
+    let time = new Date().toLocaleTimeString();
+    setCurrentTime(time);
+    setInterval(getTime, 1000);
+  }
+
+  return (
+    <div className="container">
+      <h1>{currentTime}</h1>
+      <button onClick={getTime}>Get Time</button>
+    </div>
+  );
+}
+
+export default App;
